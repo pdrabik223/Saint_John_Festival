b60e6dcca4401a0f64bea822e4eb302e874fc072
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-06-11 00:06:20 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <vector>
 
+#define DISPLAY(x) for(auto i:x) std::cout<<i;
+
 using size_t = long long unsigned int;
 
 struct Coord {
@@ -11,14 +13,24 @@ struct Coord {
     size_t x;
     size_t y;
 
+    friend std::ostream &operator<<(std::ostream &out, const Coord &me) {
+        out << me.x << "\t" << me.y << "\n";
+        return out;
+    }
 };
 
 struct linear_function {
-    linear_function(){
+    linear_function() {
         a = 0;
         b = 0;
     }
+
     linear_function(double a, double b) : a(a), b(b) {}
+
+    linear_function(Coord A, Coord B) {
+        a = (B.y - A.y) / (B.x - A.x);
+        b = A.y - a * A.x;
+    }
 
     double operator()(size_t x) {
         return a * x + b;
@@ -48,19 +60,69 @@ struct linear_function {
         return position.y >= a * position.x + b;
     }
 
+    friend std::ostream &operator<<(std::ostream &out, const linear_function &me) {
+        out << "y = " << me.a << "x + " << me.b << "\n";
+        return out;
+    }
+
     double a;
     double b;
 
 };
 
-enum side{
+enum side {
     upper = true,
     lower = false
 };
-struct triangle{
 
-    triangle(Coord A, Coord B, Coord C) {
+struct triangle {
 
+    triangle(Coord A, Coord B, Coord C) : f(A, B), g(B, C), h(C, A) {
+
+        if (f > C) for_f = upper;
+        else for_f = lower;
+        if (g > A) for_g = upper;
+        else for_g = lower;
+        if (h > B) for_h = upper;
+        else for_h = lower;
+    }
+
+    bool check_insidness(Coord P) {
+        if (for_f == upper) {
+            if (f < P)return false;
+        } else if (f > P)return false;
+
+
+        if (for_g == upper) {
+            if (g < P)return false;
+        } else if (g > P)return false;
+
+        if (for_h == upper) {
+            if (h < P)return false;
+        } else if (h > P)return false;
+
+        return true;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const triangle &me) {
+        out << me.f;
+
+        if (me.for_f) out << "/\\";
+        else out << "\\/";
+        out << "\n";
+
+        out << me.g;
+
+        if (me.for_g) out << "/\\";
+        else out << "\\/";
+        out << "\n";
+
+        out << me.h;
+
+        if (me.for_h) out << "/\\";
+        else out << "\\/";
+        out << "\n";
+        return out;
 
     }
 
@@ -101,7 +163,9 @@ int main() {
         std::cin >> x >> y;
         small_points.emplace_back(x, y);
     }
-
+    DISPLAY(big_points);
+    std::cout<<"\n";
+    DISPLAY(small_points);
 
 
     return 1;
